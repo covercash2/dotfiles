b2af6ae70739e8479d98b0ab0a4179a9c88b7476
chreeus <covercash2@gmail.com>
2017-04-18 06:30:32 -0500

    updated gitignore

--- scripts/sh/config_linux.sh
+++ scripts/sh/config_linux.sh
@@ -6,4 +6,4 @@ ln -sf "$rootdir/zshrc" $HOME/.zshrc
 ln -sf "$rootdir/zshenv" $HOME/.zshenv
 ln -sf "$rootdir/nvim" $HOME/.config/nvim
 ln -sf "$rootdir/emacs.d" $HOME/.emacs.d
-ln -sf "$rootdir/xinitrc" $HOME/.xinitr
+ln -sf "$rootdir/xinitrc" $HOME/.xinitrc 
