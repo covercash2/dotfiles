2165bab05050b7083aa8e62547d4cc2beb57cabd
chris overcash <covercash2@gmail.com>
2017-09-29 20:24:18 -0500

    fixed nvm issue and added install scripts

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+emacs=$(realpath ./emacs.d)
 env=$(realpath ./zshenv)
 rc=$(realpath ./zshrc)
 
+emacs_loc=$HOME/.emacs.d
 env_loc=$HOME/.zshenv
 rc_loc=$HOME/.zshrc
 
+ln -s $emacs $emacs_loc
 ln -s $env $env_loc
 ln -s $rc $rc_loc
+
+
