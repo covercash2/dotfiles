996de17d530e83c9e8848aaec35427d92957bc58
Chris Overcash <covercash2@gmail.com>
2017-10-03 21:29:43 -0500

    improved go script

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -109,6 +109,9 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
+;; set agenda key
+(global-set-key (kbd "C-c a") 'org-agenda)
+
 ;; font/face config
 (set-face-attribute 'default nil :height 120)
 (setq-default line-spacing 0.2)
@@ -206,3 +209,15 @@ This functions should be added to the hooks of major modes for programming."
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/notes/orgmode.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs.d/scripts/cov-go.el
+++ emacs.d/scripts/cov-go.el
@@ -1,20 +1,24 @@
-;; golang config
+;;; cov-go: --- Summary:
+;;; configurations for go development
+;;; 
+;;; Commentary:
+;;;
+;;; Code:
 
+(setenv "GOPATH" (concat (getenv "HOME") "/code/go"))
+
+(require 'company)
 (require 'go-mode)
-
-;; configure eldoc
 (require 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
 
-;; format on save
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(defun cov-go-mode-hook()
+(defun cov--go-mode-hook()
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
   (add-hook 'before-save-hook 'gofmt-before-save)
   (set (make-local-variable 'company-backends) '(company-go))
   (setq tab-width 4)
   (go-guru-hl-identifier-mode))
 
-(setenv "GOPATH" (concat (getenv "HOME") "/code/go"))
+(add-hook 'go-mode-hook 'cov--go-mode-hook)
 
 (provide 'cov-go)
+;;; cov-go.el ends here
