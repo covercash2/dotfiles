2b4cfdb7446f3d721cf8ee02a20f872ffa84bcdc
Chris Overcash <covercash2@gmail.com>
2017-10-27 08:36:29 -0500

    more cleanup

--- scripts/sh/config_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rootdir=$HOME/system/dotfiles
-
-ln -sf "$rootdir/zshrc" $HOME/.zshrc
-ln -sf "$rootdir/zshenv" $HOME/.zshenv
-ln -sf "$rootdir/nvim" $HOME/.config/nvim
-ln -sf "$rootdir/emacs.d" $HOME/.emacs.d
-ln -sf "$rootdir/xinitrc" $HOME/.xinitrc 
