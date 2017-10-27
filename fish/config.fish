61efd63bd5653a38145782d37465db75a2cc017a
Chris Overcash <covercash2@gmail.com>
2017-10-27 09:50:28 -0500

    added fish config for PATH and vi mode

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+# fish main config
+
+# set vi mode on
+fish_vi_key_bindings
+
+set cargo ~/.cargo
+set cask ~/.cask/bin
+set go ~/code/go
+set gobin $go/bin
+
+set --export GOPATH $go
+set --export CARGO_HOME $cargo
+
+set PATH $CARGO_HOME/bin $gobin $cask $PATH
