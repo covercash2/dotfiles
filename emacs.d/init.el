cdfef3ff61fc05826671a884ee5428159339b3c8
Chris Overcash <covercash2@gmail.com>
2017-11-01 12:39:38 -0500

    theming and refactoring

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -133,6 +133,9 @@
 ; set agenda key
 (global-set-key (kbd "C-c a") 'org-agenda)
 
+;; fontify (?) code blocks
+(setq org-src-fontify-natively t)
+
 ; todo keywords
 (setq org-todo-keywords
       '((sequence "TODO" "STARTED" "|" "DONE")))
@@ -196,10 +199,86 @@ This functions should be added to the hooks of major modes for programming."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3f3f3f" "#ea3838" "#7fb07f" "#fe8b04" "#62b6ea" "#e353b9" "#1fb3b3" "#d5d2be"])
+ '(custom-enabled-themes (quote (alect-black)))
+ '(custom-safe-themes
+   (quote
+    ("a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "7356632cebc6a11a87bc5fcffaa49bae528026a78637acd03cae57c091afd9b9" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" "65d9573b64ec94844f95e6055fe7a82451215f551c45275ca5b78653d505bc42" "2b6bd2ebad907ee42b3ffefa4831f348e3652ea8245570cdda67f0034f07db93" default)))
+ '(diary-entry-marker (quote font-lock-variable-name-face))
+ '(emms-mode-line-icon-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *note[] = {
+/* width height num_colors chars_per_pixel */
+\"    10   11        2            1\",
+/* colors */
+\". c #1fb3b3\",
+\"# c None s None\",
+/* pixels */
+\"###...####\",
+\"###.#...##\",
+\"###.###...\",
+\"###.#####.\",
+\"###.#####.\",
+\"#...#####.\",
+\"....#####.\",
+\"#..######.\",
+\"#######...\",
+\"######....\",
+\"#######..#\" };")))
+ '(fci-rule-color "#222222")
+ '(gnus-logo-colors (quote ("#2fdbde" "#c0c0c0")))
+ '(gnus-mode-line-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *gnus-pointer[] = {
+/* width height num_colors chars_per_pixel */
+\"    18    13        2            1\",
+/* colors */
+\". c #1fb3b3\",
+\"# c None s None\",
+/* pixels */
+\"##################\",
+\"######..##..######\",
+\"#####........#####\",
+\"#.##.##..##...####\",
+\"#...####.###...##.\",
+\"#..###.######.....\",
+\"#####.########...#\",
+\"###########.######\",
+\"####.###.#..######\",
+\"######..###.######\",
+\"###....####.######\",
+\"###..######.######\",
+\"###########.######\" };")))
  '(org-agenda-files (quote ("~/notes/orgmode.org")))
  '(package-selected-packages
    (quote
-    (fish-mode sublimity web-mode use-package restart-emacs rainbow-delimiters racer projectile powerline pallet multi-term magit kotlin-mode jedi jdee indium helm-ls-git gruvbox-theme groovy-mode gradle-mode go-guru go-eldoc git-gutter flycheck-rust exec-path-from-shell evil-surround evil-leader elpy company-jedi company-go cargo))))
+    (fish-mode sublimity web-mode use-package restart-emacs rainbow-delimiters racer projectile powerline pallet multi-term magit kotlin-mode jedi jdee indium helm-ls-git gruvbox-theme groovy-mode gradle-mode go-guru go-eldoc git-gutter flycheck-rust exec-path-from-shell evil-surround evil-leader elpy company-jedi company-go cargo)))
+ '(projectile-mode t nil (projectile))
+ '(vc-annotate-background "#222222")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#db4334")
+     (40 . "#ea3838")
+     (60 . "#abab3a")
+     (80 . "#e5c900")
+     (100 . "#fe8b04")
+     (120 . "#e8e815")
+     (140 . "#3cb370")
+     (160 . "#099709")
+     (180 . "#7fb07f")
+     (200 . "#32cd32")
+     (220 . "#8ce096")
+     (240 . "#528d8d")
+     (260 . "#1fb3b3")
+     (280 . "#0c8782")
+     (300 . "#30a5f5")
+     (320 . "#62b6ea")
+     (340 . "#94bff3")
+     (360 . "#e353b9"))))
+ '(vc-annotate-very-old-color "#e353b9"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
