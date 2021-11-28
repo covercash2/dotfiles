41518b8eff832cfea45ddb200fc24b5ba0935ddf
Chris Overcash <covercash2@gmail.com>
2021-11-28 11:52:08 -0600

    added ideavimrc

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -53,10 +53,28 @@ return require('packer').startup(function(use)
 	use 'tpope/vim-surround'
 	-- autocomplete for org mode
 	use {
-		'hrsh7th/nvim-compe',
+		'hrsh7th/nvim-cmp',
+		requires = {
+			'neovim/nvim-lspconfig',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
+			'hrsh7th/cmp-vsnip',
+			'hrsh7th/vim-vsnip',
+		},
 		config = function()
-			vim.o.completeopt = "menuone,noselect"
-			require('compe').setup {
+			vim.o.completeopt = "menu,menuone,noselect"
+			local cmp = require('cmp')
+			cmp.setup {
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'vsnip' },
+				}, { { name = 'buffer' } });
+				mapping = {
+					['<C-Space'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+				};
 				enabled = true;
 				autocomplete = true;
 				norg = true;
