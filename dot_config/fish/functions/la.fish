010d6d81be8f2cb140ef38806a6155b513ed7845
Chris Overcash <covercash2@gmail.com>
2022-03-31 16:18:55 -0400

    Remove exa dependent functions

--- dot_config/fish/functions/la.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 2
-function la --description 'List contents of directory, including hidden files in directory using long format'
-	exa -lah $argv
-end
--- dot_config/fish/functions/ls.fish
+++
@@ -1,8 +0,0 @@
-# Defined in - @ line 2
-function ls --description 'redirect ls to exa'
-	exa $argv
-	if test ! $status
-		echo "exa failed; falling back to /bin/ls"
-		/bin/ls $argv
-	end
-end
