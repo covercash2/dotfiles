27644f818bd5dabb206b4155b31140f6de9accd0
Chris Overcash - c0o02bc <Chris.Overcash@walmart.com>
2023-03-19 02:35:20 -0500

    add plenary dependency for null-ls

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -235,6 +235,7 @@ local plugins = {
 	}, -- lspconfig
 	{
 		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local null_ls = require("null-ls")
 			null_ls.setup({
