8bbe83d04ea01cb10fa91f3ed9936b61453cd308
Chris Overcash - c0o02bc <Chris.Overcash@walmart.com>
2023-03-19 02:27:48 -0500

    add null-ls to neovim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -234,6 +234,19 @@ local plugins = {
 		end
 	}, -- lspconfig
 	{
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.completion.spell,
+				}
+			})
+		end,
+	},
+	{
 		"tamago324/nlsp-settings.nvim",
 		dependencies = { "neovim/nvim-lspconfig", "williamboman/nvim-lsp-installer" },
 		config = function()
