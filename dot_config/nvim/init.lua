dd1e9a750f157dc664e4a76bcc13870b09ecdc72
Chris Overcash - c0o02bc <Chris.Overcash@walmart.com>
2023-10-26 14:42:36 -0500

    add ~/go/bin to path

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,5 @@
--- vim.api.nvim_win_set_option(0, 'number', true)
-vim.opt.number = true
--- vim.api.nvim_win_set_option(0, 'relativenumber', true)
-vim.opt.relativenumber = true
+vim.o.number = true
+--vim.o.relativenumber = true
 
 -- vim.api.nvim_set_option('smartindent', true)
 vim.opt.smartindent = true
@@ -9,20 +7,14 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
+vim.o.clipboard = 'unnamedplus'
+
 -- https://github.com/nvim-tree/nvim-tree.lua#setup
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- enable highlight groups
 vim.opt.termguicolors = true
-
-function map(mode, lhs, rhs, opts)
-	local options = { noremap = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
 
 require('plugins')
 
@@ -33,5 +25,6 @@ vim.api.nvim_create_autocmd(
 		command = [[silent! update]],
 	})
 
+-- set leader key to space
 vim.g.mapleader = " "
 
