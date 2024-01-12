1d35c8be2b909860479717cc2f5548d2440b2652
Chris Overcash - c0o02bc <Chris.Overcash@walmart.com>
2024-01-12 11:57:02 -0600

    use the new rainbow-delimiters fork

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -86,16 +86,32 @@ local plugins = {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	{
-		"HiPhish/nvim-ts-rainbow2",
+		url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				rainbow = {
-					enable = true,
-					query = "rainbow-parens",
-					strategy = require("ts-rainbow").strategy.global,
+			require("rainbow-delimiters.setup").setup({
+				strategy = {
+						[''] = rainbow_delimiters.strategy['global'],
+						vim = rainbow_delimiters.strategy['local'],
+				},
+				query = {
+						[''] = 'rainbow-delimiters',
+						lua = 'rainbow-blocks',
+				},
+				priority = {
+						[''] = 110,
+						lua = 210,
+				},
+				highlight = {
+						'RainbowDelimiterRed',
+						'RainbowDelimiterYellow',
+						'RainbowDelimiterBlue',
+						'RainbowDelimiterOrange',
+						'RainbowDelimiterGreen',
+						'RainbowDelimiterViolet',
+						'RainbowDelimiterCyan',
 				},
 			})
 		end,
