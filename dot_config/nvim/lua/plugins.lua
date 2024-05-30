37150fc6400119d0efca851a8d629d583c191863
Chris Overcash <c0o02bc@homeoffice.wal-mart.com>
2024-05-30 17:05:07 -0500

    chore(nushell): breaking change <@:)

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -30,7 +30,7 @@ local auto_show_hover = function(bufnr)
 				prefix = " ",
 				scope = "cursor",
 			}
-			vim.diagnostic.open_float(nil, opts)
+			vim.diagnostic.open_float(opts)
 		end,
 	})
 end
@@ -402,7 +402,7 @@ local plugins = {
 						local lnum = line:match("^%u%d+%s(%d+)") -- probably wrong regex
 						if lnum then
 							diags[#diags + 1] =
-									lint.diag_fmt(bufnr, tonumber(lnum) - 1, 0, lines[i]:gsub("\t", ""), 2, "djlint")
+								lint.diag_fmt(bufnr, tonumber(lnum) - 1, 0, lines[i]:gsub("\t", ""), 2, "djlint")
 						end
 					end
 					return diags
@@ -486,11 +486,11 @@ local plugins = {
 				},
 			},
 			presets = {
-				bottom_search = true,     -- use a classic bottom cmdline for search
-				command_palette = true,   -- position the cmdline and popupmenu together
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false,   -- add a border to hover docs and signature help
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 			routes = {
 				-- hide `written` messages
@@ -544,6 +544,20 @@ local plugins = {
 		end,
 	},
 	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+			"folke/neodev.nvim",
+		},
+		config = function()
+			require('neodev').setup({
+				library = { plugins = { "nvim-dap-ui" }, types = true }
+			})
+			require('dapui').setup()
+		end
+	},
+	{
 		"nvim-telescope/telescope-dap.nvim",
 		dependencies = {
 			"nvim-telescope/telescope.nvim",
@@ -579,10 +593,10 @@ local plugins = {
 				cmake_build_directory = "build",
 				cmake_generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" },
 				cmake_build_options = {},
-				cmake_console_size = 10,                                                       -- cmake output window height
-				cmake_show_console = "always",                                                 -- "always", "only_on_error"
+				cmake_console_size = 10, -- cmake output window height
+				cmake_show_console = "always", -- "always", "only_on_error"
 				cmake_dap_configuration = { name = "cpp", type = "codelldb", request = "launch" }, -- dap configuration, optional
-				cmake_dap_open_command = require("dap").repl.open,                             -- optional
+				cmake_dap_open_command = require("dap").repl.open, -- optional
 				cmake_variants_message = {
 					short = { show = true },
 					long = { show = true, max_length = 40 },
@@ -645,7 +659,7 @@ local plugins = {
 						end,
 					},
 					lualine_x = {
-						recording_symbol
+						recording_symbol,
 					},
 				},
 			})
@@ -702,18 +716,62 @@ local plugins = {
 	},
 	{
 		"mrcjkb/rustaceanvim",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
 		version = "^4",
 		ft = { "rust" },
 		lazy = false,
-		-- config = function()
-		-- 	vim.g.rustaceanvim = {
-		-- 		server = {
-		-- 			on_attach = function(client, bufnr)
-		-- 				auto_show_hover(bufnr)
-		-- 			end,
-		-- 		},
-		-- 	}
-		-- end,
+		config = function()
+			vim.g.rustaceanvim = {
+				server = {
+					on_attach = function(client, bufnr)
+						auto_show_hover(bufnr)
+					end,
+				},
+			}
+			-- https://github.com/helix-editor/helix/wiki/Debugger-Configurations#install-debuggers
+			local dap = require("dap")
+			local path = vim.fn.executable("lldb-dap")
+			if path == nil then
+				vim.print("could not find path to lldb-dap")
+			else
+				dap.adapters.lldb = {
+					type = "executable",
+					command = path,
+					name = "lldb",
+				}
+				dap.configurations.rust = {
+					initCommands = function()
+						local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+
+						local script_import = 'command script import "'
+							.. rustc_sysroot
+							.. "lib/rustlib/etc/lldb_lookup.py"
+						local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+						local commands = {}
+						local file = io.open(commands_file, "r")
+						if file then
+							for line in file:lines() do
+								table.insert(commands, line)
+							end
+							file:close()
+						end
+						table.insert(commands, 1, script_import)
+
+						return commands
+					end,
+					env = function()
+						local variables = {}
+						for k, v in pairs(vim.fn.environ()) do
+							table.insert(variables, string.format("%s=%s", k, v))
+						end
+						return variables
+					end,
+				}
+			end
+		end,
 	},
 	{
 		"ryo33/nvim-cmp-rust",
