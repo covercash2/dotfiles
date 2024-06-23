e9644217c480adee9deeacdd6af38e993569c2c1
Chris Overcash <covercash2@gmail.com>
2024-06-23 18:15:39 -0500

    chore(nixos): cleanup config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,12 +109,13 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
     wget
   ];
 
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+	environment.variables.EDITOR = "nvim";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
