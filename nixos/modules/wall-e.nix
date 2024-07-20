657aaad0fb254d26db7c53b0186895fd5365e96e
Chris Overcash <covercash2@gmail.com>
2024-07-20 11:52:08 -0500

    chore(nixos): more modularization

--- nixos/modules/wall-e.nix
+++ nixos/modules/wall-e.nix
@@ -6,8 +6,8 @@
     networking.hostName = "wall-e";
     hardware.nvidia = {
         prime = {
-            intelBusId = "PCI:1:0:0";
-	    nvidiaBusId = "PCI:0:2:0";
-	}
-    }
+          intelBusId = "PCI:1:0:0";
+          nvidiaBusId = "PCI:0:2:0";
+        };
+    };
 }
