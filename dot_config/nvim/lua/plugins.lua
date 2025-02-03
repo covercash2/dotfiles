3663e0dc94d84a653763ae07ac35a2efc4257107
Chris Overcash <covercash2@gmail.com>
2025-02-03 13:40:03 -0600

    chore(neovim): add hotkeys to markdowd-preview
    i formatted plugins.lua and it was a disaster.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -537,6 +537,13 @@ local plugins = {
 	{
 		"iamcco/markdown-preview.nvim",
 		build = "cd app; yarn install",
+    keys = {
+      {
+        "<leader>rm",
+        "<cmd>MarkdownPreview<cr>",
+        desc = "run MarkdownPreview",
+      },
+    },
 		ft = { "markdown" },
 		cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
 		init = function()
