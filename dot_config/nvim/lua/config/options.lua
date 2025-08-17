27957ca2690d065113edd516713f933ef5456541
covercash2 <covercash2@gmail.com>
2025-08-17 16:51:03 -0500

    feat(neovim): add flash plugin

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,2 +1,18 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+vim.o.relativenumber = true
+
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 10
+vim.opt.wrap = true
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.updatetime = 150
+
+vim.o.breakindent = true
