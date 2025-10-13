09d7518af45a70382f47c2dc2ece4d097c1573e6
covercash2 <covercash2@gmail.com>
2025-10-13 09:19:44 -0500

    chore(neovim): use lspmux for Rust

--- dot_config/nvim/lua/plugins/rustaceanvim.lua
+++ dot_config/nvim/lua/plugins/rustaceanvim.lua
@@ -42,12 +42,12 @@ return {
 						cargo = {
 							features = "all",
 						},
-            lspMux = {
-              version = "1",
-              method = "connect",
-              server = "rust-analyzer",
-            },
-            serverPath = vim.fn.expand('$HOME/.cargo/bin/ra-multiplex'),
+						lspMux = {
+							version = "1",
+							method = "connect",
+							server = "rust-analyzer",
+						},
+						serverPath = vim.fn.expand("$HOME/.cargo/bin/lspmux"),
 					},
 				},
 			},
