42789a293564acc158648426b81bb132bd4e8991
covercash2 <covercash2@gmail.com>
2025-10-20 09:38:43 -0500

    chore(neovim): move mini plugins to their own file

--- dot_config/nvim/lua/plugins/legacy_spec.lua
+++ dot_config/nvim/lua/plugins/legacy_spec.lua
@@ -31,36 +31,6 @@ return {
 		opts = {},
 	},
 	{
-		"nvim-mini/mini.ai",
-		version = "*",
-		opts = {},
-	},
-	{ "nvim-mini/mini.icons", version = false },
-	{
-		"nvim-mini/mini.surround",
-		version = "*",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"nvim-mini/mini.indentscope",
-		version = "*",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"nvim-mini/mini.trailspace",
-		version = "*",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"nvim-mini/mini.comment",
-		version = "*",
-		lazy = false,
-		opts = {},
-	},
-	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
 		config = true,
---
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-mini/mini.ai",
+    version = "*",
+    opts = {},
+  },
+  { "nvim-mini/mini.icons", version = false },
+  {
+    "nvim-mini/mini.surround",
+    version = "*",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "nvim-mini/mini.indentscope",
+    version = "*",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "nvim-mini/mini.trailspace",
+    version = "*",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "nvim-mini/mini.comment",
+    version = "*",
+    lazy = false,
+    opts = {},
+  },
+
+}
