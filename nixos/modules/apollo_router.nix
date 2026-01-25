33ccaf4930ec4cce9c4449846fbb354997a0bc52
Chris Overcash <covercash2@gmail.com>
2026-01-25 13:26:13 -0600

    chore(nixos): more options for the apollo_router

--- nixos/modules/apollo_router.nix
+++ nixos/modules/apollo_router.nix
@@ -24,13 +24,38 @@ with lib;
       '';
       default = 4000;
     };
+    dataDir = mkOption {
+      type = types.path;
+      description = ''
+        path to the directory where Apollo Router can store its data.
+        this directory is mounted into the container at `/mnt/space/apollo_router`.
+
+        make sure this directory exists and is writable by the `apollo_router` user.
+      '';
+      example = "/path/to/apollo_router_data";
+      default = "/mnt/space/apollo_router";
+    };
+    schema = mkOption {
+      type = types.path;
+      description = ''
+        path to the supergraph schema file.
+        this file contains the GraphQL supergraph schema
+        that Apollo Router uses to route requests.
+        the file is mounted into the container at `/dist/schema/supergraph.graphql`.
+
+        see the Apollo Router documentation for supergraph schema requirements:
+        https://www.apollographql.com/docs/graphos/routing/self-hosted/containerization/docker-router-only
+      '';
+      example = "/path/to/supergraph.graphql";
+      default = "${config.services.apollo_router.dataDir}/supergraph.graphql";
+    };
     config = mkOption {
       type = types.either types.str types.attrs;
       description = ''
         configuration for the Apollo Router.
         can be provided as either a YAML string
         or as a Nix attribute set that will be converted to YAML.
-        this configuration is written to `/mnt/space/apollo_router/router.yaml`
+        this configuration is written to `/etc/apollo_router/router.yaml`
         and mounted into the container at `/dist/config/router.yaml`.
 
         see the Apollo Router documentation for available configuration options:
@@ -62,6 +87,7 @@ with lib;
       else lib.generators.toYAML {} config.services.apollo_router.config;
 
     virtualisation.oci-containers.containers = {
+      # https://www.apollographql.com/docs/graphos/routing/self-hosted/containerization/docker-router-only
       apollo_router = {
         image = "ghcr.io/apollographql/apollo-runtime:0.0.31-PR71";
         pull = "newer";
@@ -78,9 +104,10 @@ with lib;
         # additional options can be added here if needed in the future
         extraOptions = [];
 
-        # configure volumes to mount /mnt/space/apollo_router as /dis/config
+        # configure volumes to mount configuration and supergraph schema
         volumes = [
           "/etc/apollo_router/router.yaml:/dist/config/router.yaml"
+          "${config.services.apollo_router.schema}:/dist/schema/supergraph.graphql"
         ];
       };
     };
@@ -89,7 +116,7 @@ with lib;
       isSystemUser = true;
       description = "Apollo Router user";
       group = "apollo_router";
-      home = "/mnt/space/apollo_router";
+      home = config.services.apollo_router.dataDir;
       createHome = true;
       # subUidRanges and subGidRanges can be added here if user namespace remapping becomes necessary in the future.
     };
