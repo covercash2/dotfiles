86cae86c401315f4d6f436bb9fbb9bc649717355
Chris Overcash <covercash2@gmail.com>
2026-03-21 13:25:58 -0500

    chore: minor fixes

--- modules/embedded_dev.nix
+++ modules/embedded_dev.nix
@@ -18,7 +18,7 @@
     ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", MODE:="0666"
 
     # Android ADB (was missing from NixOS config, migrated from udev/android.rules)
-    SUBSYSTEM="usb", ATTR{idVendor}=="2a70", ATTR{idProduct}=="4ee7", MODE="0660", GROUP="plugdev", SYMLINK+="android%n"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="2a70", ATTR{idProduct}=="4ee7", MODE="0660", GROUP="dialout", SYMLINK+="android%n"
 
     # STM32F3DISCOVERY rev C+ - ST-LINK/V2-1 (was missing from NixOS config, migrated from udev/99-openocd.rules)
     ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374b", MODE:="0666"
